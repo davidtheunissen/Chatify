a75aed212c7bf2100caa36bc2df4ebe43e236157
David <david.theunissen@me.com>
2024-04-24 14:05:31 +0200

    Fixed scrollToBottom() function bug on 'chatroom.html' template

--- application/chat/static/chat/js/chatroom.js
+++ application/chat/static/chat/js/chatroom.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Scroll to bottom function
     function scrollToBottom() {
         const container = document.getElementById('chat-box');
         container.scrollTop = container.scrollHeight;
     };
+
+    // Call scrollToBottom() function after chat messages are loaded
+    document.getElementById('chat_messages').addEventListener('DOMNodeInserted', function() {
+        scrollToBottom();
+    });
+
+    // Call scrollToBottom() function when page initially loads
     scrollToBottom();
 });
